0c12975559c827147dff5f6069bac3c0a9894ae1
Pegasus-E <lm1669645496@gmail.com>
2022-11-11 22:07:16 +0800

    init

---
+++ src/main/java/com/pegasus/momei/MoMeiApplication.java
@@ -0,0 +1,13 @@
+package com.pegasus.momei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoMeiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoMeiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pegasus/momei/MoMeiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pegasus.momei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoMeiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
